5a22980befac2eb76057aae04d4f9c018b1870ad
styxpilled <styxpilled.dev@gmail.com>
2023-05-28 11:08:14 +0200

    feat: keyboard page navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,20 @@
 	import '$styles/noninteractive.css';
 
 	import Header from '$comp/Header.svelte';
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+	const keybinds = async (e: KeyboardEvent) => {
+		const key = e.key.toLowerCase();
+		const route = parseInt($page.route.id?.substring(1) || '0');
+		if (key === 'arrowright') {
+			await goto(`/${route + 1}`);
+		} else if (key === 'arrowleft') {
+			await goto(`/${route > 1 ? route - 1 : route}`);
+		}
+	};
 </script>
+
+<svelte:document on:keydown={keybinds} />
 
 <div id="root" class="light">
 	<!-- <Header /> -->
